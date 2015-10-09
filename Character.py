850f9a5f9ab9dfec0bef79dd7f84096be01c312a
Jeremy Daugherty <Jeremy.Daug@gmail.com>
2015-10-09 12:10:44 -0500

    Seed code for character
    Does not have any django prep or features.

---
+++ Character.py
@@ -0,0 +1,70 @@
+#Python File for Pathfinder Character Assistant
+
+#Global lists for skills could probably be floated off into a less permanent file for ease of alteration.
+#These are dictionaries.
+STDSkills = {'acrobatics': 0, 'appraise': 0, 'bluff': 0, 'climb': 0, 'diplomacy': 0, 'disable device': 0,
+             'disguise': 0, 'escape artist': 0, 'fly': 0, 'handle animal': 0, 'heal': 0, 'intimidate': 0,
+             'linguistics': 0, 'perception': 0, 'ride': 0, 'sense motive': 0, 'slight of hand': 0,
+             'spellcraft': 0, 'stealth': 0, 'survival': 0, 'swim': 0, 'use magic device': 0}
+STDBase = {'acrobatics': 'dex', 'appraise': 'int', 'bluff': 'cha', 'climb': 'str', 'diplomacy':
+           'cha', 'disable device': 'dex', 'disguise': 'cha', 'escape artist': 'dex', 'fly': 'dex',
+           'handle animal': 'cha', 'heal': 'wis', 'intimidate': 'cha', 'linguistics': 'int',
+           'perception': 'wis', 'ride': 'dex', 'sense motive': 'wis', 'slight of hand': 'dex',
+           'spellcraft': 'int', 'stealth': 'dex', 'survival': 'wis', 'swim': 'swim',
+           'use magic device': 'cha'}
+# the variable skills have been left out (craft, Knowledge (should be added hard), proffession,
+# perform, artistry, and lore.
+
+# an optional list to allow acces to may ingore for now, it's more a test
+CondensedSkills = {'acrobatics': 0, 'athletics': 0, 'finesse': 0, 'influence': 0, 'nature': 0,
+                   'perception': 0, 'performance': 0, 'religion': 0, 'society': 0, 'spellcraft': 0,
+                   'stealth': 0, 'survival': 0}
+CondensedBase = {'acrobatics': 'dex', 'athletics': 'str', 'finesse': 'dex', 'influence': 'cha', 'nature': 'int',
+                   'perception': 'wis', 'performance': 'cha', 'religion': 'int', 'society': 'int', 'spellcraft': 'int',
+                   'stealth': 'dex', 'survival': 'wis'}
+
+# a much more common bonus to the standard skill list, that gives more skillpoints to specifically fill out these
+# skills, doesn't change anything about these skills in particular, just separates them out.
+BackgroundSkills = {'appraise', 'artistry', 'handle animal', 'linguistics', 'knowledge(engineering)',
+                    'knowledge(geography)', 'knowledge(history)', 'knowledge(nobility)', 'lore', 'perform',
+                    'profession', 'sleight of hand'}
+
+# list for trained/untrained and dict for armor penalty
+#TrainedSkills = {}
+#ArmorPenalty = {}
+
+class Character:
+        # Attributes
+        misc = {'name': 'Someone', 'player': 'no one'}
+        stats = {'str': 10, 'dex': 10, 'con': 10, 'int': 10, 'wis': 10, 'cha': 10, 'hp': 10,
+                 'maxhp': 10, 'temp': 0, 'speed': 30}
+        skills = { 'null': 0}
+        skillBase = {'null': 'NULL'}
+        ClassSkill = {'null': False}
+        #Race
+        #class
+        #feat
+        #items
+        #equipment slots
+        misc flags = 0
+        
+        #constructor
+        def __init__(self):
+                del self.skills['null']
+        
+        #functions (... means to be filled)
+                
+        def AC(self, flags, misc):
+                return 10+self.stats['dex']
+        
+        #def Attack(self, ...):
+        
+        #def Damage(self, ...)
+        
+
+
+
+#test code, comment out and ignore for your work
+i = Character()
+
+print i.skills
