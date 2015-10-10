e6093ea872ec619b1ef5efa45a0363bbc4c14fa4
Jeremy Daugherty <Jeremy.Daug@gmail.com>
2015-10-10 03:03:34 -0500

    Race File Seed

---
+++ Race.py
@@ -0,0 +1,14 @@
+#Race file for Pathfinder Roleplaying game assistant
+
+
+class Race:
+    #Attributes
+    stats = {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'cha':0}
+    size = 'med'
+    type = 'humanoid'
+    subtype = 'human'
+    speed = 30
+    startingLanguages = 'Common'
+    #traits, method of work TBD
+
+    
